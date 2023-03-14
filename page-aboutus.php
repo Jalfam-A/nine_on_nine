409ffb815a6d875e49c08a8178215e7dca0112b0
elegacion1 <e.legacion12135@gmail.com>
2023-03-14 14:43:59 -0600

    Create page-aboutus.php

---
+++ page-aboutus.php
@@ -0,0 +1,77 @@
+<?php
+/**
+ * The header for our theme
+ *
+ * This is the template that displays all of the <head> section and everything up until <div id="content">
+ *
+ * @link https://developer.wordpress.org/themes/basics/template-files/#template-partials
+ *
+ * @package nine_on_nine
+ */
+
+    get_header();
+?>
+<?php
+
+    //breadcrumbs here//
+
+    <img src="" alt=""/>
+    
+    <div>
+        <div>
+        <h4>About JALFAM Games Inc.</h4>
+        <h1>The pandemic changed the world.</h1>
+
+        <p>At JALFAM Games Inc., we recognized that Covid-19 had a significant impact on peoples mental and physical wellbeing. 
+        We wanted to create something that could bring joy, movement, and a sense of community to people s lives. Thats why we developed 9on9, 
+        a game that promotes fun, activity, and skill-building.</p>
+
+        <p>9on9 is a versatile game that can be played both indoors and outdoors, making it accessible to everyone. It involves two teams, 
+        each consisting of one to four players, who compete against each other to be the first team to reach exactly 99 points by tossing bean pucks 
+        onto a playing board. With its simple rules and fast-paced gameplay, 9on9 is a game that is easy to learn and suitable for all ages.</p>
+        
+        <p>Our goal at JALFAM Games Inc. is to create games that not only provide entertainment but also promote healthy 
+        lifestyles and social connection. We believe that through our games, we can contribute to the well-being of our players and 
+        help them overcome the challenges of these trying times. We invite you to join us in the world of 9on9 and experience the joy of movement, 
+        skill-building, and friendly competition.</p>      
+        </div>
+
+    <h2>Meet the Team</h2>
+    <img src="" alt=""/>
+    <figcaption>JALFAM</figcaption>
+
+    </div>
+
+    <div>
+        <h3>Check out our Events</h3>
+
+        <div>
+            <img src="" alt=""/>
+            <h4>EVENT</h4>
+            
+            <h3>9 on 9 Tournament - Seniors Edition</h3>
+            <p>Mon, Jan 30, 2023</p>
+
+            <div>
+                <p>See Event Deatils --></p>
+            </div>
+        </div>
+
+        <div>
+            <img src="" alt=""/>
+            <h4>EVENT</h4>
+            
+            <h3>9 on 9 Tournament - Fundraiser</h3>
+            <p>Mon, Jan 30, 2023</p>
+
+            <div>
+                <p>See Event Deatils --></p>
+            </div>
+        </div>
+
+
+
+
+
+    get_footer();
+?>
