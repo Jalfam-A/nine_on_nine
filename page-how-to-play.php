bd41a1207a20c5fdbc41e1177a785006dcd1dabc
Dazznell <56004605+dazznell@users.noreply.github.com>
2023-03-14 14:46:45 -0600

    Update homepage (complete) and how to play markup

---
+++ page-how-to-play.php
@@ -0,0 +1,129 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>How To Play</title>
+</head>
+
+<body>
+    <!-- CONTAINER -->
+    <div class="">
+
+        <!-- Breadcrumbs -->
+        <div class="">
+            <a href="" class="">Home</a>
+            <img src="" alt="Chevron Right" class="">
+            <a href="" class="">How To Play</a>
+        </div>
+
+        <!-- PAGE TITLE & NAV -->
+        <div class="">
+            <h1 class="">How To Play</h1>
+            <div class="">
+                <a href="" class="">The Setup</a>
+                <a href="" class="">The Scoring</a>
+                <a href="" class="">The Point Zones</a>
+            </div>
+        </div>
+
+        <!-- THE SETUP -->
+        <div class="">
+            <h3>The Setup</h3>
+            <div class="">
+                <a href="" class="">Step 1</a>
+                <img src="" alt="Chevron Right" class="">
+                <a href="" class="">Step 3</a>
+                <img src="" alt="Chevron Right" class="">
+                <a href="" class="">Step 2</a>
+                <img src="" alt="Chevron Right" class="">
+                <a href="" class="">Step 4</a>
+            </div>
+        </div>
+
+        <!-- SETUP STEPS -->
+        <div class="">
+
+            <!-- Setup - Step 1 -->
+            <div class="">
+
+                <!-- Step 1 - Image -->
+                <div class="">
+                    <img src="" alt="" class="">
+                </div>
+
+                <!-- Step 1 - Message -->
+                <div class="">
+                    <h2 class="">Step 1</h2>
+                    <h1 class="">Place the Board on level ground.</h1>
+                    <p class="">9on9 is a dynamic game for 1-4 players per team, suitable for indoor or outdoor play, challenging teams to reach 99 points by tossing bean pucks on the board.</p>
+                </div>
+
+            </div>
+
+
+            <!-- Setup - Step 2 -->
+            <div class="">
+
+                <!-- Step 2 - Image -->
+                <div class="">
+                    <img src="" alt="" class="">
+                </div>
+
+                <!-- Step 2 - Message -->
+                <div class="">
+                    <h2 class="">Step 2</h2>
+                    <h1 class="">Create 2 teams. Each team gets 4 bean pucks.</h1>
+                    <p class="">1 to 4 players each team. Select a colour for each team.</p>
+                </div>
+
+            </div>
+
+            <!-- Setup - Step 3 -->
+            <div class="">
+
+                <!-- Step 3 - Image -->
+                <div class="">
+                    <img src="" alt="" class="">
+                </div>
+
+                <!-- Step 3 - Message -->
+                <div class="">
+                    <h2 class="">Step 3</h2>
+                    <h1 class="">Determine who shoots first.</h1>
+                    <p class="">1 to 4 players each team. Select a colour for each team.</p>
+                </div>
+
+            </div>
+
+            <!-- Setup - Step 4 -->
+            <div class="">
+
+                <!-- Step 4 - Image -->
+                <div class="">
+                    <img src="" alt="" class="">
+                </div>
+
+                <!-- Step 4 - Message -->
+                <div class="">
+                    <h2 class="">Step 4</h2>
+                    <h1 class="">Create 2 teams. Each team gets 4 bean pucks.</h1>
+                    <p class="">1 to 4 players each team. Select a colour for each team.</p>
+                </div>
+
+            </div>
+
+        </div>
+
+
+
+
+
+    </div>
+
+
+</body>
+
+</html>
