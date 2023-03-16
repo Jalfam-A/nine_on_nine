58fd75ec083e03211f2e5ce46b106ff5a39d39e6
elegacion1 <60675547+elegacion1@users.noreply.github.com>
2023-03-16 14:22:52 -0600

    Update functions.php

--- functions.php
+++ functions.php
@@ -175,18 +175,3 @@ require get_template_directory() . '/inc/customizer.php';
 if ( defined( 'JETPACK__VERSION' ) ) {
 	require get_template_directory() . '/inc/jetpack.php';
 }
-
-function wporg_custom_post_type() {
-	register_post_type('wporg_product',
-		array(
-			'labels'      => array(
-				'name'          => __( 'Products', 'textdomain' ),
-				'singular_name' => __( 'Product', 'textdomain' ),
-			),
-			'public'      => true,
-			'has_archive' => true,
-			'rewrite'     => array( 'slug' => 'products' ), // my custom slug
-		)
-	);
-}
-add_action('init', 'wporg_custom_post_type');
