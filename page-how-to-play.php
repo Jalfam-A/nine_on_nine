d53527a60cfa88f9e0934cc5987bbf847221744b
Dazznell Martinez <martinez.dazznell@gmail.com>
2023-03-17 14:30:50 -0600

    How To Play Page Markup (Complete)

--- page-how-to-play.php
+++ page-how-to-play.php
@@ -12,12 +12,18 @@
     <!-- CONTAINER -->
     <div class="">
 
+
+
+    
         <!-- Breadcrumbs -->
         <div class="">
             <a href="" class="">Home</a>
             <img src="" alt="Chevron Right" class="">
             <a href="" class="">How To Play</a>
         </div>
+
+
+
 
         <!-- PAGE TITLE & NAV -->
         <div class="">
@@ -28,6 +34,9 @@
                 <a href="" class="">The Point Zones</a>
             </div>
         </div>
+
+
+
 
         <!-- THE SETUP -->
         <div class="">
@@ -58,7 +67,8 @@
                 <div class="">
                     <h2 class="">Step 1</h2>
                     <h1 class="">Place the Board on level ground.</h1>
-                    <p class="">9on9 is a dynamic game for 1-4 players per team, suitable for indoor or outdoor play, challenging teams to reach 99 points by tossing bean pucks on the board.</p>
+                    <p class="">9on9 is a dynamic game for 1-4 players per team, suitable for indoor or outdoor play,
+                        challenging teams to reach 99 points by tossing bean pucks on the board.</p>
                 </div>
 
             </div>
@@ -119,6 +129,84 @@
 
 
 
+        <!-- SCORING STEPS -->
+        <div class="">
+
+            <!-- Scoring - Step 1 -->
+            <div class="">
+
+                <!-- Step 1 - Image -->
+                <div class="">
+                    <img src="" alt="" class="">
+                </div>
+
+                <!-- Step 1 - Message -->
+                <div class="">
+                    <h2 class="">Step 1</h2>
+                    <h1 class="">Throw 1 bean puck at a time alternating teams until all have been thrown.</h1>
+                    <p class="">This completes a round.</p>
+                </div>
+
+            </div>
+
+
+
+
+            <!-- Scoring - Step 2 -->
+            <div class="">
+
+                <!-- Step 2 - Image -->
+                <div class="">
+                    <img src="" alt="" class="">
+                </div>
+
+                <!-- Step 2 - Message -->
+                <div class="">
+                    <h2 class="">Step 2</h2>
+                    <h1 class="">At the end of each round, each team adds their score to the previous round.</h1>
+                    <p class="">The team with the most points after each round throws first in the next round. </p>
+                    <p class="">If there is a tie, the winner from the previous round goes first.</p>
+                </div>
+
+            </div>
+
+        </div>
+
+
+
+
+        <!-- THE POINT ZONES -->
+        <div class="">
+
+            <!-- Image of the board with labels -->
+            <div class="">
+                <img src="" alt="" class="">
+            </div>
+
+            <!-- Image of the scoring instructions and notes -->
+            <div class="">
+                <img src="" alt="" class="">
+                <img src="" alt="" class="">
+            </div>
+
+        </div>
+
+
+        <!-- Buy Now CTA -->
+        <div class="">
+
+            <!-- CTA Message -->
+            <div class="">
+                <h2 class="">A game for fun, movement and skill!</h2>
+                <h1 class="">9on9</h1>
+                <p class="">9on9 is an outdoor or indoor game where 2 teams, with 1 to 4 players per team, challenge each other to be the first team to get to 99 points exactly by tossing bean pucks on to the playing board.</p>
+            </div>
+
+            <!-- CTA Image -->
+            <div class="">
+                <img src="" alt="" class="">
+            </div>
+        </div>
 
 
     </div>
