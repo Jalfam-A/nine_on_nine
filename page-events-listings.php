07ac809dd65732ea6c8b1b6fa35016b30c807275
elegacion1 <e.legacion12135@gmail.com>
2023-04-11 14:51:20 -0600

    Update page-events-listings.php

--- page-events-listings.php
+++ page-events-listings.php
@@ -1,272 +1,59 @@
 <?php
 /**
- * This is the page-events-listings-us.php file
- *
- *
+ * The template for displaying all pages.
+ * 
+ * This is the template that displays all pages by default. Please note that this is the WordPress construct of pages and that other 'pages' on your WordPress site may use a different template.
+ * 
  * @link https://developer.wordpress.org/themes/basics/template-hierarchy/
- *
- * @package nine_on_nine
  * 
- * 
- * 
+ * @package nine-on-nine
  */
-
-get_header();
 ?>
 
+<?php get_header(); ?>
+<!-- <h2>This is page-recipes.php </h2> -->
 
-    <section class="event-listing-page">
-        <div class=""> 
-            <div class="event-listing-top-cards"> <!--top cards start-->
-                <div class="">
-                    <h2>Upcoming Events</h2>
-                </div>
-                <div class="event-listing-top-cards-one"> <!--First card start-->
-                <div>
-        <img src="<?php echo esc_url(get_template_directory_uri() .'/assets/img/events-listing-page/chinese-new-years-tournament.jpg' ); ?>" alt="an image of the nine-on-nine board" class="chinese-new-years-image-file">
+<div class="recipes-flex-container">
+    <div class="recipes-container"></div>
+        <h2>All Recipes</h2>
+        
+        <div class="flex-inner-container">
+            <div class="flex-recipes">
+                <?php
+                $args = array(
+                    'post_type'      => 'events',
+                    'posts_per_page' => 16,
+                );
+                $loop = new WP_Query($args);
+                while ( $loop->have_posts() ) {
+                    $loop->the_post();
+                    get_template_part ('template-parts/content', 'events');
+                }
+                ?>
+            </div>
+        </div>
+        
     </div>
-    <div class="chinese-new-years-text">
-        <div>
-            <h4>EVENT</h4>
+    
+<?php get_footer(); ?>
 
-            <h2>9 on 9 Tournament - Chinese New Years</h2>
-            <div class ="chinese-new-years-description">
-                <p>Happy Chinese New Year! We welcome you to the 9 on 9 Tournament - Chinese New Year Edition. Hope you can all attend and have fun. All ages welcome!</p>
-
-                <div class="event-details">
-                    <div class="details-first-row">
-                        <div class="time">
-                            <p>10:00 AM</p>
-                        </div> <!--end of div-->
-                        <div class="date">
-                            <p>Wed, Feb 01, 2023</p>
-                        </div> <!--end of div-->
-                    </div>
-                    <div class="details-second-row">
-                        <div class="location">
-                            <p>Picnic Site #2, Hawrelak Park</p>
-                        </div> <!--end of div-->
-                        <div class="event-price">
-                            <p>$10</p>
-                        </div> <!--end of div-->
-                    </div>
-                </div><!--end of event-details-page-->
-                        <div class="link-to-event-detail-page">
-                            <a href="#" class="see-event-details">SEE EVENT DETAILS <img src="<?php echo esc_url(get_template_directory_uri() . '/assets/img/icons/chevron.png'); ?>" alt="Right Arrow" class="icon"></a>
-                        </div>
-            </div> <!--end of chinese-new-years-description-->
-            
-                    </div>
-                </div> <!--end of chinese-new-years-text-->
-                
-                </div> <!--First card end-->
-                <div class="event-listing-top-cards-two"> <!--Second card start-->
-                    <div class="">
-                        <img src="<?php echo esc_url(get_template_directory_uri() .'/assets/img/events-listing-page/university-edition-tournament.jpg' ); ?>" alt="an image of the 9 on 9 board" class="">
-                    </div>
-                    <div class="">
-                        <div class="">
-                            <h4>Event</h4>
-                        </div>
-                        <div class=""> <!--Main text section-->
-                            <h2>9 on 9 Tournament - University Edition</h2>
-                        </div>
-                        <div class="">
-                            <p>Think your skills surpass your friends? Then show them what you can do in the official
-                                9on9 Tournament - University Edition. Hope you can all attend and have fun. All ages welcome!
-                                Brings lot of water though.</p>
-                        </div>
-                        <div class="event-details"> <!--time, date, place, price-->
-                            <div class="details-first-row">
-                                <div class="time">
-                                    <p>11:00 AM</p>
-                                </div>
-                                <div class="date">
-                                    <p>Wed, Feb 22, 2023</p>
-                                </div>
-                            </div>
-                            <div class="details-second-row">
-                                <div class="place">
-                                    <p>SUB, University of Alberta</p>
-                                </div>
-                                <div class="price">
-                                    <p>$10</p>
-                                </div>
-                            </div>
-                        </div>
-                        <div class="link-to-event-detail-page">
-                            <a href="#" class="see-event-details">SEE EVENT DETAILS <img src="<?php echo esc_url(get_template_directory_uri() . '/assets/img/icons/chevron.png'); ?>" alt="Right Arrow" class="icon"></a>
-                        </div>
-                    </div>
-                </div> <!--Second card end-->
-            </div> <!--top cards end-->
-            <div class="event-listing-bottom-cards">
-                <div class="">
-                    <h3>Previous News and Events</h3>
-                </div>
-                <div class=""> <!--event bottom cards-->
-                    <div class="events-listing-bottom-cards-top"> <!--top-layer event bottom cards-->
-                        <a href="#">
-                        <div class="bottom-card-one"> <!--card 1-->
-                            
-                            <div class=""> <!--image side of card-->
-                                <img src="<?php echo esc_url(get_template_directory_uri() .'/assets/img/events-listing-page/400-games-sold.jpg' ); ?>" alt="two people standing between boxes of 9 on 9 game that have been sold" class="four-hundred-games-image-file">
-                            </div>
-                            <div class="events-listing-bottom-cards-text"> <!--text side of card-->
-                                <div class="">
-                                    <div class="">
-                                        <h4>NEWS</h4>
-                                    </div>
-                                    <div class="">
-                                        <h3>400 games sold in Edmonton</h3>
-                                    </div>
-                                </div>
-                                <div class="event-listing-bottom-card-date">
-                                    <p>01/30/2023</p>
-                                </div>
-                            </div>
-                        </div> <!--end of card 1-->
-                        </a>
-                        <a href="#">
-                        <div class="bottom-card-two"> <!--card 2-->
-                        
-                            <div class=""> <!--image side of card-->
-                                <img src="<?php echo esc_url(get_template_directory_uri() .'/assets/img/events-listing-page/all-ages-welcome.jpg' ); ?>" alt="an image showing that 9 on 9 is good for both young and old." class="all-ages-welcome-file">
-                            </div>
-                            <div class="events-listing-bottom-cards-text"> <!--text side of card-->
-                                <div class="">
-                                    <div class="">
-                                        <h4>EVENT</h4>
-                                    </div>
-                                    <div class="">
-                                        <h3>9 on 9 Tournament - All Ages Welcome!</h3>
-                                    </div>
-                                </div>
-                                <div class="event-listing-bottom-card-date">
-                                    <p>01/30/2023</p>
-                                </div>
-                            </div>
-                        
-                        </div> <!--end of card 2-->
-                        </a>
-                    </div> <!--end of top-layer event bottom cards-->
-
-
-                    <div class="events-listing-bottom-cards-middle"> <!--middle-layer event bottom cards-->
-                    <a href="#">
-                        <div class="bottom-card-three"> <!--card 3-->
-                        
-                            <div class=""> <!--image side of card-->
-                                <img src="<?php echo esc_url(get_template_directory_uri() .'/assets/img/events-listing-page/holiday-madness.jpg' ); ?>" alt="an image of presents" class="holiday-madness-file">
-                            </div>
-                            <div class="events-listing-bottom-cards-text"> <!--text side of card-->
-                                <div class="">
-                                    <div class="">
-                                        <h4>NEWS</h4>
-                                    </div>
-                                    <div class="">
-                                        <h3>Holiday Madness</h3>
-                                    </div>
-                                </div>
-                                <div class="event-listing-bottom-card-date third-card-listing">
-                                    <p>01/30/2023</p>
-                                </div>
-                            </div>
-                        
-                        </div> <!--end of card 3-->
-                        </a>
-                        <a href="#">
-                        <div class="bottom-card-four"> <!--card 4-->
-                        
-                            <div class=""> <!--image side of card-->
-                                <img src="<?php echo esc_url(get_template_directory_uri() .'/assets/img/events-listing-page/black-friday-sales.jpg' ); ?>" alt="9 on 9 black Friday sale" class="black-friday-sales-file">
-                            </div>
-                            <div class="events-listing-bottom-cards-text"> <!--text side of card-->
-                                <div class="">
-                                    <div class="">
-                                        <h4>NEWS</h4>
-                                    </div>
-                                    <div class="">
-                                        <h3>Black Friday Sales</h3>
-                                    </div>
-                                </div>
-                                <div class="event-listing-bottom-card-date fourth-card-listing">
-                                    <p>01/30/2023</p>
-                                </div>
-                            </div>
-                        
-                        </div> <!--end of card 4-->
-                        </a>
-                    </div> <!--end of middle-layer event bottom cards-->
-
-
-                    <div class="events-listing-bottom-cards-bottom"> <!--bottom-layer event bottom cards-->
-                        <a href="#">
-                        <div class="bottom-card-five"> <!--card 5-->
-                        
-                            <div class=""> <!--image side of card-->
-                                <img src="<?php echo esc_url(get_template_directory_uri() .'/assets/img/events-listing-page/seniors-edition-tournament.jpg' ); ?>" alt="an image of senior citizens winning awards from the tournament" class="seniors-edition-file">
-                            </div>
-                            <div class="events-listing-bottom-cards-text"> <!--text side of card-->
-                                <div class="">
-                                    <div class="">
-                                        <h4>EVENT</h4>
-                                    </div>
-                                    <div class="">
-                                        <h3>9 on 9 Tournament - Seniors Edition</h3>
-                                    </div>
-                                </div>
-                                <div class="event-listing-bottom-card-date">
-                                    <p>01/30/2023</p>
-                                </div>
-                            </div>
-                        
-                        </div> <!--end of card 5-->
-                        </a>
-                        <a href="#">
-                        <div class="bottom-card-six"> <!--card 6-->
-                        
-                            <div class=""> <!--image side of card-->
-                                <img src="<?php echo esc_url(get_template_directory_uri() .'/assets/img/events-listing-page/fundraiser-tournament.jpg' ); ?>" alt="An image of a person getting douse by water for charity" class="fundraiser-file">
-                            </div>
-                            <div class="events-listing-bottom-cards-text"> <!--text side of card-->
-                                <div class="">
-                                    <div class="">
-                                        <h4>EVENT</h4>
-                                    </div>
-                                    <div class="">
-                                        <h3>9 on 9 Tournament - Fundraiser</h3>
-                                    </div>
-                                </div>
-                                <div class="event-listing-bottom-card-date">
-                                    <p>01/30/2023</p>
-                                </div>
-                            </div>
-                        
-                        </div> <!--end of card 6-->
-                        </a>
-                    </div> <!--end of bottom-layer event bottom cards-->
-                </div> <!--end of event bottom cards-->
-            </div>
-        </div>
-
-        <div class=""> <!--Previous and Next Updates button-->
-            <div class="">
-                <svg></svg>
-                <p>Previous Updates</p>
-            </div>
-            <div class="">
-                <svg></svg>
-                <p>Next Updates</p>
-            </div>
-        </div>
-        
-        
-    </section>
-
-<!-- 
-    Add comments to all html later
--->
 
 <?php
-get_footer();
+$args = array{
+    'post_type' => 'events_content'
+    'post_per_page' => -1,
+};
+
+&loop = new WP_Query($args);
+
+if($loop->have_post()) {
+    while ($loop->have_post()) {
+        $loop->the_post();
+
+        the_post_thumbnail('medium');
+        the_title();
+        the_excerpt();
+
+    }
+}
+?>
