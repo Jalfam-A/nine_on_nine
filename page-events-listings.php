a77c6536d6c1f066fbf1914ea7ed4fff8f23b582
elegacion1 <e.legacion12135@gmail.com>
2023-04-14 14:33:48 -0600

    Update page-events-listings.php

--- page-events-listings.php
+++ page-events-listings.php
@@ -1,36 +1,61 @@
 <?php
 /**
- * This is the page-events-listings-us.php file
- *
- *
+ * The template for displaying all pages.
+ * 
+ * This is the template that displays all pages by default. Please note that this is the WordPress construct of pages and that other 'pages' on your WordPress site may use a different template.
+ * 
  * @link https://developer.wordpress.org/themes/basics/template-hierarchy/
- *
- * @package nine_on_nine
  * 
- * 
- * 
+ * @package nine-on-nine
  */
-
-get_header();
 ?>
 
-
-    <section class="event-listing-page">
-        <div class=""> 
-            <div class=""> <!--top cards start-->
-                <div class="">
-                    <h2>Upcoming Events</h2>
-                </div>
-                <div class=""> <!--First card start-->
-                    <?php get_template_part('template-parts/content', 'events')?>
-                </div>
+<?php get_header(); ?>
+<!-- <h2>This is page-recipes.php </h2> -->
+<!--
+<div class="recipes-flex-container">
+    <div class="recipes-container"></div>
+        <h2>All Recipes</h2>
         
-    </section>
+        <div class="flex-inner-container">
+            <div class="flex-recipes">
+                
+                <?php
+                
+                $args = array(
+                    'post_type'      => 'events',
+                    'posts_per_page' => 16,
+                );
+                $loop = new WP_Query($args);
+                while ( $loop->have_posts() ) {
+                    $loop->the_post();
+                    get_template_part ('template-parts/content', 'events');
+                }
+                ?>*/
+            </div>
+        </div>
+        
+    </div>
+    
+<?php get_footer(); ?>
 
-<!-- 
-    Add comments to all html later
--->
 
 <?php
-get_sidebar();
-get_footer();
+$args = array{
+    'post_type' => 'events_content'
+    'post_per_page' => -1,
+};
+
+&loop = new WP_Query($args);
+
+if($loop->have_post()) {
+    while ($loop->have_post()) {
+        $loop->the_post();
+
+        the_post_thumbnail('medium');
+        the_title();
+        the_excerpt();
+
+    }
+}
+?>
