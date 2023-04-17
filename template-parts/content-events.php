5d35652c6ea99170c41072a4a00366bcd99fdf04
elegacion1 <60675547+elegacion1@users.noreply.github.com>
2023-04-16 20:59:15 -0600

    Update content-events.php

--- template-parts/content-events.php
+++ template-parts/content-events.php
@@ -23,10 +23,16 @@
                     <p><?php the_category(); ?></p>
                     <p><?php the_title('<h3 class="entry-title">', '</h3>'); ?></p>
                     <p><?php the_field('short_description'); ?></p>
-                    <p><?php the_field('event_time'); ?></p>
-                    <p><?php the_field('event_date'); ?></p>
-                    <p><?php the_field('event_location'); ?></p>
-                    <p>$ <?php the_field('price'); ?></p>
+                    <div class="time-date-location-price" id="time-date-location-price">
+                        <div>
+                        <p><?php the_field('event_time'); ?></p>
+                        <p><?php the_field('event_date'); ?></p>
+                        </div>
+                        <div>
+                        <p><?php the_field('event_location'); ?></p>
+                        <p>$ <?php the_field('price'); ?></p>
+                        </div>
+                    </div>
                 </div>
                  <!-- <a href="#" class="see-event-details">SEE EVENT DETAILS <img src="<//?php echo esc_url(get_template_directory_uri() . '/assets/img/icons/chevron.png'); ?>" alt="Right Arrow" class="icon"></a> -->
             </a>
