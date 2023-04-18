929e8f0d9a479bfb09ebdf03a2b67dea3dba84ef
elegacion1 <e.legacion12135@gmail.com>
2023-04-18 14:07:42 -0600

    Update page-about-us.php

--- page-about-us.php
+++ page-about-us.php
@@ -66,13 +66,13 @@ get_header();
         </div><!--end of top section-->
         
         <div class="inner-container event-details-lower-container">
-            <div class="current-events"><h3>Current Events</h3></div>
+            <div class="current-events"><h3>Check out our Events</h3></div>
             <div class="event-details-lower-inner-container">
                 <?php
                 $args = array(
                     'post_type'      => 'upcoming-events',
                     'posts_per_page' => 2,
-                    'offset'         => 2, // Skip the first two posts
+                    
                     
                 );
                 $loop = new WP_Query($args);
